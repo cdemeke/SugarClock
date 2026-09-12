5c98caf86d8080b0bc9bf897c97359005447b909
Chris Demeke <demeke2@gmail.com>
2026-09-11 22:45:48 -0400

    Revert "Add option to pause auto-cycle during night mode"
    This reverts commit d68c7854ba2e8969efad0b4641c55f7bdf77bf80.

--- src/glucose_engine.cpp
+++ src/glucose_engine.cpp
@@ -1034,11 +1034,7 @@ void engine_loop() {
 
     // Auto-cycle display modes
     AppConfig& cfg = config_get();
-    bool cycling_active = cfg.auto_cycle_enabled && toggle_count > 1 && !connection_info_visible;
-    if (cycling_active && is_night_mode() && cfg.night_disable_auto_cycle) {
-        cycling_active = false;
-    }
-    if (cycling_active) {
+    if (!connection_info_visible && cfg.auto_cycle_enabled && toggle_count > 1) {
         unsigned long cycle_interval_ms = (unsigned long)cfg.auto_cycle_sec * 1000UL;
         if (last_cycle_ms == 0) last_cycle_ms = millis();
         if (millis() - last_cycle_ms >= cycle_interval_ms) {
